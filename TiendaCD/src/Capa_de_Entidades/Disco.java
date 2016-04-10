29f194ebb5462516f9a281fc5e81531483f3b0a1
Sofía <Sofa@Sofa-PC>
2016-04-10 17:05:47 -0300

    Clases Disco, Usuario y Venta creadas.

---
+++ TiendaCD/src/Capa_de_Entidades/Disco.java
@@ -0,0 +1,42 @@
+package Capa_de_Entidades;
+
+public class Disco {
+	private int codDisco;
+	private String titulo;
+	private int añoLanzamiento;
+	private int cantCopiasDisp;
+	private float precio;
+	
+	public int getCodDisco() {
+		return codDisco;
+	}
+	public void setCodDisco(int codDisco) {
+		this.codDisco = codDisco;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public int getAñoLanzamiento() {
+		return añoLanzamiento;
+	}
+	public void setAñoLanzamiento(int añoLanzamiento) {
+		this.añoLanzamiento = añoLanzamiento;
+	}
+	public int getCantCopiasDisp() {
+		return cantCopiasDisp;
+	}
+	public void setCantCopiasDisp(int cantCopiasDisp) {
+		this.cantCopiasDisp = cantCopiasDisp;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	
+
+}
