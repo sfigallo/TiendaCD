4a244df33876a7fd1b0c2d225c3332c6720d524a
Sofía <Sofa@Sofa-PC>
2016-04-10 16:54:53 -0300

    Clase Autor creada.

---
+++ TiendaCD/src/Capa_de_Entidades/Autor.java
@@ -0,0 +1,19 @@
+package Capa_de_Entidades;
+
+public class Autor {
+	private int codAutor;
+	private String nombreAutor;
+	public int getCodAutor() {
+		return codAutor;
+	}
+	public void setCodAutor(int codAutor) {
+		this.codAutor = codAutor;
+	}
+	public String getNombreAutor() {
+		return nombreAutor;
+	}
+	public void setNombreAutor(String nombreAutor) {
+		this.nombreAutor = nombreAutor;
+	}
+	
+}
