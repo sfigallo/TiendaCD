64487258dc65713caac79d00dd7ea63a44ec9d5d
ramirojimenez094 <ramirojimenez094@gmail.com>
2016-04-14 11:34:05 -0300

    Agregados métodos de manejo de BD.

--- TiendaCD/src/Capa_de_Control/Controlador.java
+++ TiendaCD/src/Capa_de_Control/Controlador.java
@@ -10,7 +10,7 @@ public class Controlador {
 	public ArrayList<Disco> buscarDiscos(String cadena, int clase){
 		switch(clase){
 			case '1':
-				return DataDiscos.getDiscosxNombre(cadena);
+				return DataDiscos.getDiscosxTitulo(cadena);
 			case '2':
 				return DataDiscos.getDiscosxAutor(cadena);
 			case '3':
