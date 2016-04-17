a3eb907be813d94880f9fbc7b37d409c18ddfae3
Ramiro Jimenez <ramirojimenez094@gmail.com>
2016-04-16 23:19:07 -0300

    Handle barra de busqueda de DiscosAdmin

--- TiendaCD/src/Capa_de_Presentacion/DiscosAdmin.java
+++ TiendaCD/src/Capa_de_Presentacion/DiscosAdmin.java
@@ -1,11 +1,17 @@
 package Capa_de_Presentacion;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Capa_de_Control.Controlador;
+import Capa_de_Entidades.ContenedorArrayList;
+import Capa_de_Entidades.Disco;
 
 /**
  * Servlet implementation class DiscosAdmin
@@ -34,8 +40,17 @@ public class DiscosAdmin extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		Controlador con = new Controlador();
+		if(request.getParameter("eventoBuscar")!=null){
+			String cadena = request.getParameter("titBuscar");
+			if (!cadena.isEmpty()) {
+				ArrayList<Disco> discos = con.buscarDiscos(cadena);
+				ContenedorArrayList cont = new ContenedorArrayList();
+				cont.setDiscos(discos);
+				request.setAttribute("contenedorDiscos", cont);
+			}
+			request.getRequestDispatcher("discosAdmin.jsp").forward(request, response);
+		}
 	}
 
 }
