5372fb20d47e722003437e018a5bc5f55c8a081a
Sofía <Sofa@Sofa-PC>
2016-04-20 11:02:46 -0300

    Registro e Inicio de Sesión de usuarios.

--- TiendaCD/src/Capa_de_Presentacion/Inicio.java
+++ TiendaCD/src/Capa_de_Presentacion/Inicio.java
@@ -1,11 +1,17 @@
 package Capa_de_Presentacion;
 
 import java.io.IOException;
+import java.sql.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Capa_de_Control.Controlador;
+import Capa_de_Entidades.TipoUsuario;
+import Capa_de_Entidades.Usuario;
 
 /**
  * Servlet implementation class Index
@@ -33,8 +39,46 @@ public class Inicio extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		Usuario usu = new Usuario();
+		Controlador con = new Controlador();
+		if(request.getParameter("eventoIngresar")!=null){
+			usu = con.buscarUsuario(request.getParameter("userregister"));
+			if(usu!=null){
+				String contraseña = usu.getContraseña();
+				if(contraseña.equals(request.getParameter("password"))){
+					request.getSession().setAttribute("usuario", usu);
+					if(usu.getTipo().getCodTipoUsuario() == TipoUsuario.COD_ADMIN)
+						request.getRequestDispatcher("discosAdmin.jsp").forward(request, response);
+					if(usu.getTipo().getCodTipoUsuario() == TipoUsuario.COD_USER)
+						request.getRequestDispatcher("discos.jsp").forward(request, response);
+				}
+				else{
+					request.setAttribute("msjError", "El usuario o la contraseña son incorrectos.");
+					request.getRequestDispatcher("inicio.jsp").forward(request, response);
+				}
+			}
+			else
+				request.getRequestDispatcher("inicio.jsp").forward(request, response);
+		}
+		if(request.getParameter("eventoRegistrar")!=null){
+			usu = con.buscarUsuario(request.getParameter("user"));
+			if(usu == null){
+				usu = new Usuario();
+				usu.setUsuario(request.getParameter("user"));
+				usu.setContraseña(request.getParameter("password"));
+				usu.setNombre(request.getParameter("nombre"));
+				usu.setApellido(request.getParameter("apellido"));
+				usu.setFechaNac(Date.valueOf(request.getParameter("fechanac")));
+				usu.setDni(Integer.parseInt(request.getParameter("dni")));
+				con.nuevoUsuario(usu);
+				request.getSession().setAttribute("usuario", usu);
+				request.getRequestDispatcher("discos.jsp").forward(request, response);
+			}
+			else{
+				request.setAttribute("msjError", "El usuario ya existe.");
+				request.getRequestDispatcher("inicio.jsp").forward(request, response);
+			}	
+		}
 	}
 
 }
